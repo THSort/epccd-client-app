393fa070d6923e7d4aa17c968b913bb42436f05d
THSort <23100056@lums.edu.pk>
2025-03-04 23:17:03 +0500

    create empty air-quality-detailed-report screen

---
+++ frontend/screens/air-quality-detailed-report/air-quality-detailed-report.tsx
@@ -0,0 +1,13 @@
+import type { ReactElement } from "react";
+import React from "react";
+import { View } from "react-native";
+import type { AirQualityDetailedReportProps } from "./air-quality-detailed-report.types";
+import { styles } from "./air-quality-detailed-report.styles";
+
+export function AirQualityDetailedReport({ ...props }: AirQualityDetailedReportProps): ReactElement {
+    return (
+        <View style={styles.container}>
+            {/* Add your component code here */}
+        </View>
+    );
+}
---
+++ frontend/screens/air-quality-detailed-report/air-quality-detailed-report.types.ts
@@ -0,0 +1,3 @@
+export interface AirQualityDetailedReportProps {
+    // Add props here
+}
