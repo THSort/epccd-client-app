b828e66a16ee279a8d19a3b0f714f608b073da2a
THSort <23100056@lums.edu.pk>
2025-03-04 23:17:29 +0500

    create empty air-quality-detailed-report screen

--- frontend/screens/air-quality-detailed-report/air-quality-detailed-report.tsx
+++ frontend/screens/air-quality-detailed-report/air-quality-detailed-report.tsx
@@ -1,10 +1,10 @@
-import type { ReactElement } from "react";
-import React from "react";
-import { View } from "react-native";
-import type { AirQualityDetailedReportProps } from "./air-quality-detailed-report.types";
-import { styles } from "./air-quality-detailed-report.styles";
+import type {ReactElement} from 'react';
+import React from 'react';
+import {View} from 'react-native';
+import type {AirQualityDetailedReportProps} from './air-quality-detailed-report.types';
+import {styles} from './air-quality-detailed-report.styles';
 
-export function AirQualityDetailedReport({ ...props }: AirQualityDetailedReportProps): ReactElement {
+export function AirQualityDetailedReport({...props}: AirQualityDetailedReportProps): ReactElement {
     return (
         <View style={styles.container}>
             {/* Add your component code here */}
