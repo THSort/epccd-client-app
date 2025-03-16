6706231e91a530984dd2c80318da8dd39fd46cd7
THSort <23100056@lums.edu.pk>
2025-03-16 15:31:39 +0500

    delete backend code related to historical data

--- backend/src/routes/epaMonitorsData.routes.ts
+++ backend/src/routes/epaMonitorsData.routes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { getCurrentEpaMonitorsDataForLocation, getPollutantSummaryForLocation, getHistoricalEpaMonitorsDataForLocation } from "../controllers/epaMonitorsData.controller";
+import { getCurrentEpaMonitorsDataForLocation } from "../controllers/epaMonitorsData.controller";
 
 const router = express.Router();
 
 // The more specific routes should come first
-router.get("/epaMonitorsData/summary/:location", getPollutantSummaryForLocation);
-router.get("/epaMonitorsData/historical/:location", getHistoricalEpaMonitorsDataForLocation);
+// router.get("/epaMonitorsData/summary/:location", getPollutantSummaryForLocation);
+// router.get("/epaMonitorsData/historical/:location", getHistoricalEpaMonitorsDataForLocation);
 router.get("/epaMonitorsData/:location", getCurrentEpaMonitorsDataForLocation);
 
 export default router;
