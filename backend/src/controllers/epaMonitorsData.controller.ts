dcabf666adaab91c3978b6aa3c01dd5bbba3419f
THSort <23100056@lums.edu.pk>
2025-03-17 22:59:40 +0500

    wip

--- backend/src/controllers/epaMonitorsData.controller.ts
+++ backend/src/controllers/epaMonitorsData.controller.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from "express";
-import {fetchCurrentEpaMonitorsDataForLocation} from "../services/epaMonitorsData.service";
+import {fetchCurrentEpaMonitorsDataForLocation, getAllPollutantHistoricalData} from "../services/epaMonitorsData.service";
 import logger from "../utils/logger";
 
 const getCurrentEpaMonitorsDataForLocation = async (req: Request, res: Response): Promise<void> => {
@@ -19,6 +19,25 @@ const getCurrentEpaMonitorsDataForLocation = async (req: Request, res: Response)
     }
 };
 
+const getHistoricalPollutantData = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const location = Number(req.params.location);
+        const currentDateTime = new Date();
+
+        const historicalData = await getAllPollutantHistoricalData(location, currentDateTime);
+
+        res.json(historicalData);
+    } catch (error) {
+        if (error instanceof Error) {
+            logger.error(`Error fetching historical pollutant data for location ${req.params.location}: ${error.message}`);
+        } else {
+            logger.error(`Unknown error while fetching historical pollutant data for location ${req.params.location}`);
+        }
+        res.status(500).json({message: "Failed to fetch historical pollutant data"});
+    }
+};
+
 export {
     getCurrentEpaMonitorsDataForLocation,
+    getHistoricalPollutantData,
 };
--- backend/src/routes/epaMonitorsData.routes.ts
+++ backend/src/routes/epaMonitorsData.routes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { getCurrentEpaMonitorsDataForLocation } from "../controllers/epaMonitorsData.controller";
+import { getCurrentEpaMonitorsDataForLocation, getHistoricalPollutantData } from "../controllers/epaMonitorsData.controller";
 
 const router = express.Router();
 
 // The more specific routes should come first
 // router.get("/epaMonitorsData/summary/:location", getPollutantSummaryForLocation);
-// router.get("/epaMonitorsData/historical/:location", getHistoricalEpaMonitorsDataForLocation);
+router.get("/epaMonitorsData/historical/:location", getHistoricalPollutantData);
 router.get("/epaMonitorsData/:location", getCurrentEpaMonitorsDataForLocation);
 
 export default router;
