c062d43967d1678f9864df2bf0ee2faea3fd5f1f
THSort <23100056@lums.edu.pk>
2025-06-04 01:00:34 +0500

    fix

--- backend/src/controllers/version.controller.ts
+++ backend/src/controllers/version.controller.ts
@@ -4,7 +4,7 @@ import {Request, Response} from "express";
 const LATEST_APP_VERSION = "1.0.1";
 
 // URL where users can download the latest APK - update this with your actual APK URL
-const LATEST_APK_URL = "https://drive.google.com/file/d/149RJzLTTOGNSquW5Io-wpm2xahihTM73/view?usp=drive_link";
+const LATEST_APK_URL = "https://drive.google.com/file/d/182KGzQTwXCEYvHRt99yYCTUINp5oDoTf/view?usp=drive_link";
 
 /**
  * Check if the provided app version is the latest
